a52b1f59236e8ba4f0dbd19afd0c03175cb3c429
techx1 <ss.uitech01@gmail.com>
2023-01-31 12:29:31 +0500

    feat: add poppins font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '../../styles/globals.css'
+import '../../styles/globals.css';
+import { Poppins } from '@next/font/google';
+
+const poppins = Poppins({ subsets: ['latin'], weight: '400' , display: "fallback",});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
