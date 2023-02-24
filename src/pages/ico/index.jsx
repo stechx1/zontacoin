ad253088ce4ca21398ae7dc2a42bca333f8d65f8
techx1 <ss.uitech01@gmail.com>
2023-02-25 02:18:37 +0500

    mobile version ico page

--- src/pages/ico/index.jsx
+++ src/pages/ico/index.jsx
@@ -8,7 +8,7 @@ const IcoPage = () => {
     <div className='container mx-auto'>
       <div className='flex flex-col justify-center items-center my-20 space-y-8'>
         <h2 className='text-3xl font-bold'>Buy ZontaCoin</h2>
-        <div className='bg-gray-700 w-1/2 p-8 rounded'>
+        <div className='bg-gray-700 w-[85%] md:w-1/2 p-8 rounded'>
           <p className='mb-8'>1 Zonta Coin = 0.08 USDT</p>
           <label
             class='block uppercase tracking-wide text-white text-xs font-bold mb-2'
